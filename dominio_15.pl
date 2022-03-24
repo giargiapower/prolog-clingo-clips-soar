767bf05484e40f552f09e81bc28580a9c289f695
sp213779 <molinari.gianni@educ.di.unito.it>
2022-03-24 14:02:27 +0000

    Upload New File

---
+++ dominio_15.pl
@@ -0,0 +1,8 @@
+num_col(4).
+num_righe(4).
+%pos(riga, colonna, tessera).
+%come formalizzare gli stati. sono una configurazione del 15 ?
+%non posso inserire le posizioni dei numeri nel dominio in quanto devono mutare col movimento
+%delle caselle 
+% prima ipotesi è di indicare uno stato del 15 con una lista ma mi sembra troppo complicato da 
+%utilizzare
