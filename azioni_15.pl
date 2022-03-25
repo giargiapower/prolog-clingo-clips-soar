8d6328708e9c2e39f5e4c3e1d3e2bc50243c001b
sp213779 <molinari.gianni@educ.di.unito.it>
2022-03-25 11:07:44 +0000

    prima bozza controllo azioni e funzione trasforma

--- azioni_15.pl
+++ azioni_15.pl
@@ -1,4 +1,17 @@
 %applicabile(AZ,S)
+applicabile(nord, pos(Riga, Colonna, Cella)):- Cella is 0,
+Riga>0.
 
+applicabile(sud, pos(Riga, Colonna, Cella)):- Cella is 0,
+num_righe(NR),
+Riga<NR.
+
+applicabile(est, pos(Riga, Colonna, Cella)):- Cella is 0,
+num_col(NC),
+Colonna<NC.
+
+applicabile(ovest, pos(Riga, Colonna, Cella)):- Cella is 0,
+Colonna>0.
 
 %trasforma(AZ, S, NUOVO_S)
+trasforma(nord, pos(Riga, Colonna, 0),...).
