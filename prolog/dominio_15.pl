98013e96d92021ed529a297fa80361237a0db1d5
gianni <gianni.molinari@studenti.unimi.it>
2022-04-02 17:29:51 +0200

    modifica dominio

--- prolog/dominio_15.pl
+++ prolog/dominio_15.pl
@@ -1,3 +1,4 @@
+:- dynamic([pos/2]).
 num_col(4).
 num_righe(4).
 %pos(riga, colonna, tessera).
@@ -5,20 +6,9 @@ num_righe(4).
 % aggiungere i nuovi valori nelle celle e rimuovere quelli vecchi che non valgono più.
 % es |-|1| --> sposto 1 ovvero pos(0, 1, 1)  a sinistra e quindi aggiorno quel pos e metto quello nuovo
 % -> pos(0,0,1)  , pos(0 ,1, 0).
-pos(0,0,6).
-pos(0,1,13).
-pos(0,2,1).
-pos(0,3,7).
-pos(1,0,0).
-pos(1,1,9).
-pos(1,2,4).
-pos(1,3,12).
-pos(2,0,10).
-pos(2,1,2).
-pos(2,2,11).
-pos(2,3,14).
-pos(3,0,3).
-pos(3,1,5).
-pos(3,2,15).
-pos(3,3,8).
+iniziale([8,0,9,7,14,4,5,3,6,15,13,10,1,2,11,12]).
+e([3, 3, 2, 3, 2, 2, 2, 4, 4, 2, 1, 1, 3, 3, 2, 5]).
+pos(0,1).
+%stato finale ?somma distanze da loro posizione corretta = 0
 %ci sarà da inserire una euristica 
+%[3, 3, 2, 3, 2, 2, 2, 4, 4, 2, 1, 1, 3, 3, 2, 5].
